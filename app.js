8d1be577172f465a89d137bd567b46ac0a8b2e11
Oleksiy <oleksiymykhalevich86@gmail.com>
2022-01-13 00:59:25 +0200

    complete slider

--- app.js
+++ app.js
@@ -1,12 +1,60 @@
-const productsCountEl = document.getElementById("products-count");
-const addToCartsBtns = document.querySelectorAll(".btn-cart");
 
-for (let i = 0; i < addToCartsBtns.length; i++) {
-    addToCartsBtns[i].addEventListener("click", function () {
-      let prevProductCount = productsCountEl.textContent ;
-      productsCountEl.textContent = +productsCountEl.textContent +1
-      
+const productsCountEl = document.getElementById("products-count");
+console.log(productsCountEl);
+
+const addToCartBtns = document.querySelectorAll(".btn-cart");
+console.log(addToCartBtns);
+
+
+for (let i = 0; i < addToCartBtns.length; i++) {
+    addToCartBtns[i].addEventListener("click", function () {
+      productsCountEl.textContent = +productsCountEl.textContent + 1;
     });
   };
 
- 
+
+// change like state
+const likeBtns = document.querySelectorAll(".like");
+likeBtns.forEach((item) => {
+  item.addEventListener("click", function () {
+    item.classList.toggle("liked");
+  });
+});
+
+
+//modal 
+const modal = document.querySelector(".modal");
+const moreDetailsBtns = document.querySelectorAll(".btn-details");
+const btnClose = document.querySelector(".btn-close");
+
+function openModal() {
+  modal.classList.add("show");
+  modal.classList.remove("hide");
+}
+function closeModal() {
+  modal.classList.add("hide");
+  modal.classList.remove("show");
+}
+
+moreDetailsBtns.forEach((item) => {
+  item.addEventListener("click", openModal);
+});
+
+btnClose.addEventListener("click", closeModal);
+
+
+  $(".slider").slick({
+    dots: true,
+  })
+    
+    
+
+
+
+
+
+
+
+
+
+
