fd6c233b4cccaf4e41f973943ca61c4825eb43f7
Oleksiy <oleksiymykhalevich86@gmail.com>
2022-01-12 16:26:29 +0200

    D:/Git/

---
+++ shop-project-master/app.js
@@ -0,0 +1,28 @@
+// const productsCountEl = document.getElementById("products-count");
+// const addToCartsBtns = document.querySelectorAll(".btn-cart");
+// for (let i = 0; i < addToCartsBtns.length; i++) {
+//     addToCartsBtns[i].addEventListener("click", function (n) {
+//     });
+//   };
+
+let i = 0;
+function clicked(n) {
+console.log(i)
+    let productsCountEl = document.getElementById("products-count");
+    i = i + n;
+    productsCountEl.innerHTML = i;
+};
+window.onclick;
+
+
+// const likeBTN = document.getElementsByClassName("small-btn");
+
+// likeBTN.addEventListener("click" , function () {
+//      likeBTN.style.backgroundColor = "red";
+     
+// })
+document.querySelectorAll('.small-btn').forEach(function(e) {
+    e.addEventListener('click', function() {
+      this.style.backgroundColor = "#2c71b8";
+    })
+  });
