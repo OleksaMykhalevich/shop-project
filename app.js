4edc4a388ae173dbcc8af11d7e2001e633df7274
Oleksiy <oleksiymykhalevich86@gmail.com>
2022-01-12 19:26:31 +0200

    D:/Git/

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-// const productsCountEl = document.getElementById("products-count");
-// const addToCartsBtns = document.querySelectorAll(".btn-cart");
-// for (let i = 0; i < addToCartsBtns.length; i++) {
-//     addToCartsBtns[i].addEventListener("click", function (n) {
-//     });
-//   };
+const productsCountEl = document.getElementById("products-count");
+const addToCartsBtns = document.querySelectorAll(".btn-cart");
 
-let i = 0;
-function clicked(n) {
-console.log(i)
-    let productsCountEl = document.getElementById("products-count");
-    i = i + n;
-    productsCountEl.innerHTML = i;
-};
-window.onclick;
+for (let i = 0; i < addToCartsBtns.length; i++) {
+    addToCartsBtns[i].addEventListener("click", function () {
+      let prevProductCount = productsCountEl.textContent ;
+      productsCountEl.textContent = +prevProductCount +1
+      
+    });
+  };
+
+// let i = 0;
+// function clicked(n) {
+// console.log(i)
+//     let productsCountEl = document.getElementById("products-count");
+//     i = i + n;
+//     productsCountEl.innerHTML = i;
+// };
+// window.onclick;
 
 
 // const likeBTN = document.getElementsByClassName("small-btn");
@@ -20,9 +24,11 @@ window.onclick;
 // likeBTN.addEventListener("click" , function () {
 //      likeBTN.style.backgroundColor = "red";
      
-// })
-document.querySelectorAll('.small-btn').forEach(function(e) {
-    e.addEventListener('click', function() {
-      this.style.backgroundColor = "#2c71b8";
-    })
-  });
+
+
+// document.querySelectorAll('.small-btn').forEach(function(e) {
+//     e.addEventListener('click', function() {
+     
+//       this.style.backgroundColor = "#2c71b8";
+//     })
+//   });   
