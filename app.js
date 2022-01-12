1c08d4899637dec5bd1b1ad409fcc2f1e94b761e
Oleksiy <oleksiymykhalevich86@gmail.com>
2022-01-12 19:29:51 +0200

    D:/Git/

--- app.js
+++ app.js
@@ -4,31 +4,9 @@ const addToCartsBtns = document.querySelectorAll(".btn-cart");
 for (let i = 0; i < addToCartsBtns.length; i++) {
     addToCartsBtns[i].addEventListener("click", function () {
       let prevProductCount = productsCountEl.textContent ;
-      productsCountEl.textContent = +prevProductCount +1
+      productsCountEl.textContent = +productsCountEl.textContent +1
       
     });
   };
 
-// let i = 0;
-// function clicked(n) {
-// console.log(i)
-//     let productsCountEl = document.getElementById("products-count");
-//     i = i + n;
-//     productsCountEl.innerHTML = i;
-// };
-// window.onclick;
-
-
-// const likeBTN = document.getElementsByClassName("small-btn");
-
-// likeBTN.addEventListener("click" , function () {
-//      likeBTN.style.backgroundColor = "red";
-     
-
-
-// document.querySelectorAll('.small-btn').forEach(function(e) {
-//     e.addEventListener('click', function() {
-     
-//       this.style.backgroundColor = "#2c71b8";
-//     })
-//   });   
+ 
